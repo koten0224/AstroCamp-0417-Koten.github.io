ab4fcf4d77230cfa40afe967fcb0dcaa30ea9d05
koten0224 <koten0224@gmail.com>
2020-04-18 21:49:52 +0800

    debug little

--- album.js
+++ album.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#paste').on('click', function(){
     let url = $("#pic_url").val()
     if(/^http.+\.(jpg|jpeg|png|gif)$/.test(url)){
-      $("#album").append(`<div class="picture col-4 p-1"><img class="w-100" src=${url} alt=""></div>`)
+      $("#album").prepend(`<div class="picture col-4 p-1"><img class="w-100" src=${url} alt=""></div>`)
     }else{
       $('.pic-alert').show()
     }
