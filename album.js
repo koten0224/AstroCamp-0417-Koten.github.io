9675cb2bab3f1e06cb3f9b7bad37d88c8eddeac8
koten0224 <koten0224@gmail.com>
2020-04-18 11:56:12 +0800

    rename

--- album.js
+++ album.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
   $('#paste').on('click', function(){
     let url = $("#pic_url").val()
     if(/^http.+\.(jpg|jpeg|png|gif)$/.test(url)){
-      $("#album").append(`<div class="pathetic col-4 p-1"><img class="w-100" src=${url} alt=""></div>`)
+      $("#album").append(`<div class="picture col-4 p-1"><img class="w-100" src=${url} alt=""></div>`)
     }else{
       $('.pic-alert').show()
     }
     $("#pic_url").val("")
   })
 
-  $('#album').on('click','.pathetic' ,function() {
+  $('#album').on('click','.picture' ,function() {
     $(this).hide()
   })
 
