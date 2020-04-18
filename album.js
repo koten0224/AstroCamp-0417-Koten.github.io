afe228340220f323345c68a59c482e8f6027bea7
koten0224 <koten0224@gmail.com>
2020-04-18 23:49:55 +0800

    add random function

--- album.js
+++ album.js
@@ -1,8 +1,16 @@
+let randomPicID = function (){
+  return Math.floor(Math.random() * 1085 )
+}
 $(document).ready(function() {
-  $('#paste').on('click', function(){
-    let url = $("#pic_url").val()
+  $('button').on('click', function(){
+    let url
+    if($(this).attr('id') === 'paste'){
+      url = $("#pic_url").val()
+    }else{
+      url = `https://i.picsum.photos/id/${randomPicID()}/300/300.jpg`
+    }
     if(/^http.+\.(jpg|jpeg|png|gif)$/.test(url)){
-      $("#album").prepend(`<div class="picture col-4 p-1"><img class="w-100" src=${url} alt=""></div>`)
+      $("#album").prepend(`<div class="picture col-6 col-sm-4 p-1"><img class="w-100" src=${url} alt=""></div>`)
     }else{
       $('.pic-alert').show()
     }
